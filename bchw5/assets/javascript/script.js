99eb05186515957f8f60d6e39414a43c56cb93b7
eric <ericgip97@gmail.com>
2020-05-21 18:39:20 -0700

    finished local storage, event handling

--- bchw5/assets/javascript/script.js
+++ bchw5/assets/javascript/script.js
@@ -1,9 +1,64 @@
 $(document).ready(function() {
 
-    getCurrentDay();
+    // on page load
+    doInit();
 
-    pastPresentFuture();
+    // on page load, if i have anything, i want to load it.
+    function doInit() {
 
+        getCurrentDay();
+
+        pastPresentFuture();
+
+        retrieveDescriptions();
+
+    }
+
+
+    // button event handlers
+
+    $(".save-btn").on("click", function() {
+        // this == binded to the button / class
+        // event listeners return an object
+
+        // we need to differentiate all the save buttons, do this with data-type
+
+        // console.log($(this));
+        
+        // getting time id from description tag / class.
+        const timeId = $(this).siblings(".descriptions").attr("data-time");
+        console.log(timeId);
+
+        // getting value from user input. 
+        // using .val because taking the value from it, not the attribute.
+        const userInput = $(this).siblings(".descriptions").val();
+        console.log(userInput);
+
+        // setting to local storage.
+        // this will store in console > application > local storage.
+        localStorage.setItem(timeId, userInput);
+    });
+
+
+    // ======= functions here =======
+    
+    function retrieveDescriptions() {
+        // going to target descriptions class again, its going to return an array
+
+        const descriptions = $(".descriptions");
+
+        $(descriptions).each(function (i, element) {
+            // get time Id to retrieve data from local storage
+            const timeId = $(element).attr("data-time")
+
+            const descriptions = localStorage.getItem(timeId)
+            console.log(timeId);
+            console.log(descriptions);
+
+            $(element).text(descriptions);
+        });
+    }
+    
     function getCurrentDay(){
 
         // getting current day formatted with the .format()
@@ -23,23 +78,33 @@ $(document).ready(function() {
         const descriptions = $(".descriptions");
 
         // when you tag more than one class, it's stored in an array. 
-        console.log($(descriptions[0]).attr("data-time"));
+        // console.log($(descriptions[0]).attr("data-time"));
 
         // getting current time that's stored on the description block. 
-            // however, returns as string, we need to parse int. 
+        // however, returns as string, we need to parse int. 
+        // const currentTimeOfBlock = parseInt($(descriptions[0]).attr("data-time"));
+        // console.log(descriptions[1]);
 
-        const currentTimeOfBlock = parseInt($(descriptions[0]).attr("data-time"));
-        console.log(descriptions[1]);
+        // .each not ForEach
 
-        if (currentTimeOfBlock < currentHour) {
-            $(descriptions[0]).addClass("past");
-        } 
-        else if (currentTimeOfBlock === currentHour) {
-            $(descriptions[0]).addClass("present");
-        }
-        else {
-            $(descriptions[0]).addClass("future");
-        }
+        $(descriptions).each(function(index, element) {
+            console.log(element)
+
+            const currentTimeOfBlock = parseInt($(element).attr("data-time"));
+
+            if (currentTimeOfBlock < currentHour) {
+                $(element).addClass("past");
+            } 
+            else if (currentTimeOfBlock === currentHour) {
+                $(element).addClass("present");
+            }
+            else {
+                $(element).addClass("future");
+            }
+        })
+
+
+        
     }
 
 })
